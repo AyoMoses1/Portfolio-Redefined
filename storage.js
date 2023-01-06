c750ef0447896ed928984f98cfb46d0657c3474b
Ayo Moses <ayomoses@afrimash.com>
2023-01-06 11:41:03 +0100

    Fixed local storage integration

---
+++ storage.js
@@ -0,0 +1,22 @@
+const contactForm = document.querySelector('#footer_form');
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const obj = {
+    fullName: document.querySelector('#name').value,
+    emailAdress: document.querySelector('#email').value,
+    message: document.querySelector('#textarea').value,
+  };
+
+  localStorage.setItem('formEntry', JSON.stringify(obj));
+});
+
+const retrievedObject = localStorage.getItem('formEntry');
+const getValue = JSON.parse(retrievedObject);
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('formEntry')) {
+    document.querySelector('#name').value = getValue.fullName;
+    document.querySelector('#email').value = getValue.emailAdress;
+    document.querySelector('#message').value = getValue.message;
+  }
+});
